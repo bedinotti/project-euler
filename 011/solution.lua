66e778222b86cc307d70f48a9741d971ecbca76b
Chris Downie <cdownie@gmail.com>
2020-01-19 14:19:23 -0700

    [011] Create 011 problem from template.

---
+++ 011/solution.lua
@@ -0,0 +1,24 @@
+-- run `method` with any passed argument. Print result to method and time to run.
+function benchmark(method, ...)
+  local startTime = os.clock()
+  print(method(...))
+  local endTime = os.clock()
+  print(string.format("Solved in %.2fs", endTime - startTime))
+end
+
+-- Example function to show off `benchmark`
+function wasteTime(countTo)
+  print("called with " .. countTo)
+  local dict = {}
+  for i=1, countTo do
+    dict[i] = i
+  end
+
+  while #dict > 0 do
+    table.remove(dict, 1)
+  end
+  return 
+end
+
+-- example of `benchmark`
+benchmark(wasteTime, 100000)
