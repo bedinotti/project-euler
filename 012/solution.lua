66991cc7385c90f211188ce74dde034e107584f8
Chris Downie <cdownie@gmail.com>
2020-01-20 07:55:09 -0700

    [012] Another approach in lua, also slow.

--- 012/solution.lua
+++ 012/solution.lua
@@ -6,15 +6,59 @@ function benchmark(method, ...)
   print(string.format("Solved in %.2fs", endTime - startTime))
 end
 
+-- From solution 10
+function primesLessThan(upperBound)
+  return coroutine.wrap(function ()
+    local allNumbers = {}
+    for i = 1, upperBound - 1 do
+      allNumbers[#allNumbers + 1] = i
+    end
+    local maxIndex = #allNumbers
+
+    local index = 2
+    repeat
+      local nextPrime = allNumbers[index]
+      coroutine.yield(nextPrime)
+
+      -- print("eliminating multiples of " .. nextPrime)
+      for multiples = 2 * nextPrime, maxIndex, nextPrime do
+        -- local matchingIndex = multiples
+        -- print("eliminating index " .. matchingIndex .. " value " .. allNumbers[matchingIndex])
+        allNumbers[multiples] = nil
+      end
+
+      repeat
+        index = index + 1
+      until allNumbers[index] ~= nil or index > maxIndex
+      -- print("index is" .. index .. "for maxindex" .. maxIndex)
+    until index > maxIndex
+  end)
+end
+
 function divisors(number)
-  local divs = {}
-  for i=1, number do
-    if number % i == 0 then
-      divs[#divs + 1] = i
+  if number == 1 then
+    return 1
+  end
+
+  local count = 1
+  -- print("primes less than " .. (number+1))
+  for prime in primesLessThan(number + 1) do
+    -- print("prime is" .. prime)
+    if number % prime == 0 then
+      -- print("trying prime " .. prime .. "for number " .. number)
+      local exponent = 0
+      local copy = number
+      while copy % prime == 0 do
+        copy = copy / prime
+        exponent = exponent + 1
+      end
+
+      -- print("  " .. prime .. "^" .. exponent)
+      count = count * (exponent + 1)
     end
   end
 
-  return #divs
+  return count
 end
 
 function triangleWith(divisorMin)
@@ -34,3 +78,12 @@ function triangleWith(divisorMin)
 end
 
 benchmark(triangleWith, 500)
+
+-- print(divisors(6))
+-- print(1, divisors(1) == 1)
+-- print(3, divisors(3) == 2)
+-- print(6, divisors(6) == 4)
+-- print(10, divisors(10) == 4)
+-- print(15, divisors(15) == 4)
+-- print(21, divisors(21) == 4)
+-- print(28, divisors(28) == 6)
