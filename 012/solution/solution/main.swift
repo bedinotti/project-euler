5d350301696d45a481cfbf84b2daaa94dd272262
Chris Downie <cdownie@gmail.com>
2020-01-20 19:04:32 -0700

    [012] Add swift solution.

--- 012/solution/solution/main.swift
+++ 012/solution/solution/main.swift
@@ -17,8 +17,42 @@ func benchmark(method: () -> Void) {
     print(String(format: "Solved in %.2fs", end - start))
 }
 
-benchmark {
-    for i in 0..<1_000_000 {
-        print(i)
+func divisorCount(of n: Int) -> Int {
+    guard n != 1 else {
+        return 1
     }
+    var i = 2
+    var remainder = n
+    var count = 1
+    while i < n && remainder > 1 {
+        defer {
+            i += 1
+        }
+        guard remainder % i == 0 else {
+            continue
+        }
+
+        var exponent = 0
+        while remainder % i == 0 {
+            exponent += 1
+            remainder /= i
+        }
+        count *= (exponent + 1)
+    }
+    return count
+}
+
+func divisibleTriangle(minDivisors: Int) -> Int {
+    var triangleNum = 1
+    var i = 1
+    while divisorCount(of: triangleNum) < minDivisors {
+        i += 1
+        triangleNum += i
+    }
+    return triangleNum
+}
+
+benchmark {
+    let result = divisibleTriangle(minDivisors: 500)
+    print(result)
 }
