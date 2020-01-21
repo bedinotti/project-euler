0053a012b15442d8196cf738be6e4cb978d38bd6
Chris Downie <cdownie@gmail.com>
2020-01-20 17:57:09 -0700

    [012] improved benchmark passthrough function

--- 012/solution.lua
+++ 012/solution.lua
@@ -1,9 +1,14 @@
+-- Store in this table, rather than in the global namespace
+global = {}
+
 -- run `method` with any passed argument. Print result to method and time to run.
 function benchmark(method, ...)
   local startTime = os.clock()
-  print(method(...))
+  local values = {method(...)}
   local endTime = os.clock()
   print(string.format("Solved in %.2fs", endTime - startTime))
+  print(table.unpack(values))
+  return table.unpack(values)
 end
 
 -- From solution 10
@@ -20,17 +25,13 @@ function primesLessThan(upperBound)
       local nextPrime = allNumbers[index]
       coroutine.yield(nextPrime)
 
-      -- print("eliminating multiples of " .. nextPrime)
       for multiples = 2 * nextPrime, maxIndex, nextPrime do
-        -- local matchingIndex = multiples
-        -- print("eliminating index " .. matchingIndex .. " value " .. allNumbers[matchingIndex])
         allNumbers[multiples] = nil
       end
 
       repeat
         index = index + 1
       until allNumbers[index] ~= nil or index > maxIndex
-      -- print("index is" .. index .. "for maxindex" .. maxIndex)
     until index > maxIndex
   end)
 end
@@ -41,11 +42,8 @@ function divisors(number)
   end
 
   local count = 1
-  -- print("primes less than " .. (number+1))
   for prime in primesLessThan(number + 1) do
-    -- print("prime is" .. prime)
     if number % prime == 0 then
-      -- print("trying prime " .. prime .. "for number " .. number)
       local exponent = 0
       local copy = number
       while copy % prime == 0 do
@@ -53,7 +51,6 @@ function divisors(number)
         exponent = exponent + 1
       end
 
-      -- print("  " .. prime .. "^" .. exponent)
       count = count * (exponent + 1)
     end
   end
@@ -70,14 +67,20 @@ function triangleWith(divisorMin)
     end
   end)
 
+  local count = 1
   for tri in triangles do
     if divisors(tri) > divisorMin then
       return tri
     end
+    if count % 100 == 0 then
+      print(string.format("Looking at %dth number", count))
+    end
+    count = count + 1
+
   end
 end
 
-benchmark(triangleWith, 500)
+-- benchmark(triangleWith, 500)
 
 -- print(divisors(6))
 -- print(1, divisors(1) == 1)
@@ -87,3 +90,12 @@ benchmark(triangleWith, 500)
 -- print(15, divisors(15) == 4)
 -- print(21, divisors(21) == 4)
 -- print(28, divisors(28) == 6)
+
+local nineThousand = 0
+for i = 1, 9000 do
+  nineThousand = nineThousand + i
+end
+print(nineThousand)
+
+result = benchmark(divisors, nineThousand)
+print("and result is " .. result)
