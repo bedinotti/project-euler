3ffe3ce16f69ab92399004c8f8ff07f81cec68d7
Chris Downie <cdownie@gmail.com>
2020-01-20 18:54:00 -0700

    [012] Lua solution finally solves in <2s

--- 012/solution.lua
+++ 012/solution.lua
@@ -12,52 +12,56 @@ function benchmark(method, ...)
 end
 
 -- From solution 10
-function primesLessThan(upperBound)
-  return coroutine.wrap(function ()
-    local allNumbers = {}
-    for i = 1, upperBound - 1 do
-      allNumbers[#allNumbers + 1] = i
-    end
-    local maxIndex = #allNumbers
+-- function primesLessThan(upperBound)
+--   return coroutine.wrap(function ()
+--     local allNumbers = {}
+--     for i = 1, upperBound - 1 do
+--       allNumbers[#allNumbers + 1] = i
+--     end
+--     local maxIndex = #allNumbers
 
-    local index = 2
-    repeat
-      local nextPrime = allNumbers[index]
-      coroutine.yield(nextPrime)
+--     local index = 2
+--     repeat
+--       local nextPrime = allNumbers[index]
+--       coroutine.yield(nextPrime)
 
-      for multiples = 2 * nextPrime, maxIndex, nextPrime do
-        allNumbers[multiples] = nil
-      end
+--       for multiples = 2 * nextPrime, maxIndex, nextPrime do
+--         allNumbers[multiples] = nil
+--       end
 
-      repeat
-        index = index + 1
-      until allNumbers[index] ~= nil or index > maxIndex
-    until index > maxIndex
-  end)
-end
+--       repeat
+--         index = index + 1
+--       until allNumbers[index] ~= nil or index > maxIndex
+--     until index > maxIndex
+--   end)
+-- end
 
-function divisors(number)
-  if number == 1 then
-    return 1
-  end
+function altDivisors(n)
+  if n == 1 then return 1 end
 
-  local count = 1
-  for prime in primesLessThan(number + 1) do
-    if number % prime == 0 then
+  local divisorCount = 1
+
+  local i = 2
+  local originalN = n
+  while n > 1 and i <= originalN do
+    if n % i == 0 then
       local exponent = 0
-      local copy = number
-      while copy % prime == 0 do
-        copy = copy / prime
+
+      while n % i == 0 do
         exponent = exponent + 1
+        n = n / i
       end
 
-      count = count * (exponent + 1)
+      divisorCount = divisorCount * (exponent + 1)
     end
+
+    i = i + 1
   end
 
-  return count
+  return divisorCount
 end
 
+-- actual function to solve
 function triangleWith(divisorMin)
   local triangles = coroutine.wrap(function ()
     local sum = 0
@@ -69,33 +73,14 @@ function triangleWith(divisorMin)
 
   local count = 1
   for tri in triangles do
-    if divisors(tri) > divisorMin then
+    if altDivisors(tri) > divisorMin then
       return tri
     end
     if count % 100 == 0 then
       print(string.format("Looking at %dth number", count))
     end
     count = count + 1
-
   end
 end
 
--- benchmark(triangleWith, 500)
-
--- print(divisors(6))
--- print(1, divisors(1) == 1)
--- print(3, divisors(3) == 2)
--- print(6, divisors(6) == 4)
--- print(10, divisors(10) == 4)
--- print(15, divisors(15) == 4)
--- print(21, divisors(21) == 4)
--- print(28, divisors(28) == 6)
-
-local nineThousand = 0
-for i = 1, 9000 do
-  nineThousand = nineThousand + i
-end
-print(nineThousand)
-
-result = benchmark(divisors, nineThousand)
-print("and result is " .. result)
+benchmark(triangleWith, 500)
