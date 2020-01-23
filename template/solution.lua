a32c05e97d7de98adfc0a64165a5db9d1c19aa28
Chris Downie <cdownie@gmail.com>
2020-01-22 21:05:29 -0700

    [template] Add ping function to lua template.

--- template/solution.lua
+++ template/solution.lua
@@ -1,26 +1,22 @@
 -- run `method` with any passed argument. Print result to method and time to run.
 function benchmark(method, ...)
-  local startTime = os.clock()
+  startTime = os.clock()
   local values = {method(...)}
   local endTime = os.clock()
-  print(string.format("Solved in %.2fs", endTime - startTime))
+  print(string.format("Solved in %.4fs", endTime - startTime))
   print(table.unpack(values))
   return table.unpack(values)
 end
 
--- Example function to show off `benchmark`
-function wasteTime(countTo)
-  print("called with " .. countTo)
-  local dict = {}
-  for i=1, countTo do
-    dict[i] = i
-  end
+function ping(...)
+  local diff = os.clock() - startTime
+  print(string.format("%.4fs", diff), ...)
+end
 
-  while #dict > 0 do
-    table.remove(dict, 1)
-  end
-  return 
+-- Example function
+function solve(arg1)
+  return "solution"
 end
 
 -- example of `benchmark`
-benchmark(wasteTime, 100000)
+benchmark(solve, 100000)
