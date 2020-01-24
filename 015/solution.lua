8c82415b4c3e4c76a93c2509ebc6fec4584a4ee8
Chris Downie <cdownie@gmail.com>
2020-01-24 07:06:11 -0700

    [015] Clean up fast math lua solution. Solves instantly.

--- 015/solution.lua
+++ 015/solution.lua
@@ -1,3 +1,4 @@
+-- Benchmark methods. These help measure performance & make sure the function keeps running
 -- run `method` with any passed argument. Print result to method and time to run.
 function benchmark(method, ...)
   startTime = os.clock()
@@ -13,42 +14,12 @@ function ping(...)
   print(string.format("%.4fs", diff), ...)
 end
 
--- First approach: find all the paths via depth-first search
-function makeGrid(size) 
-  local grid = {}
-  for i=1, size+1 do
-    local column = {}
-    for j=1, size+1 do
-      column[j] = true
-    end
-    grid[i] = column
-  end
-  return grid
-end
-
-
-function depthFirstSearch(grid, x, y, pathCount)
-  local point = (grid[x] or {})[y]
-
-  if point == nil then
-    return 0
-  end
-  if x == #grid and #grid[x] == y then
-    return 1
-  end
-
-  return depthFirstSearch(grid, x+1, y, pathCount) + depthFirstSearch(grid, x, y+1, pathCount)
-end
-
-function solve(squareSize) 
-  local grid = makeGrid(squareSize)
-  return depthFirstSearch(grid, 1, 1, 0)
-end
-
--- Second approach - count the # of paths to the diagonal.
+-- Problem solution
 function fastSolve(squareSize)
   local vertexCount = squareSize + 1
-  -- Start with the second row. Simpler than having the base case in the loop.
+
+  -- The first row is one vertex: {1}. the second is 2 vertices, with 1 path to each: {1, 1}
+  -- Start with the second row state and start iterating on the 3rd row.
   local vertexPathCount = {1, 1}
   for i = 3, vertexCount do
     local newPathCount = {1}
@@ -59,6 +30,7 @@ function fastSolve(squareSize)
     vertexPathCount = newPathCount
   end
 
+  -- We're at the diagonal. Multiply the # of paths at each vertex and add them up.
   local sum = 0
   for i = 1, #vertexPathCount do
     sum = sum + (vertexPathCount[i] ^ 2)
@@ -67,11 +39,4 @@ function fastSolve(squareSize)
   return sum
 end
 
-
--- for i=1, 20 do
---   print("solving " .. i)
---   benchmark(solve, i)
--- end
 benchmark(fastSolve, 20)
--- print(1, fastSolve(1), fastSolve(1) == 2)
--- print(2, fastSolve(2), fastSolve(2) == 6)
