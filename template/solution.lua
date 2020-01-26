fe99d130e8014bf05f26fa7c2aa6d97a9effbad4
Chris Downie <cdownie@gmail.com>
2020-01-25 21:31:46 -0700

    [template] Refactor common helper lua functions into separate file.

--- template/solution.lua
+++ template/solution.lua
@@ -1,22 +1,8 @@
--- run `method` with any passed argument. Print result to method and time to run.
-function benchmark(method, ...)
-  startTime = os.clock()
-  local values = {method(...)}
-  local endTime = os.clock()
-  print(string.format("Solved in %.4fs", endTime - startTime))
-  print(table.unpack(values))
-  return table.unpack(values)
-end
+local helpers = loadfile("helpers.lua")()
 
-function ping(...)
-  local diff = os.clock() - startTime
-  print(string.format("%.4fs", diff), ...)
-end
-
--- Example function
+-- Example solution function
 function solve(arg1)
   return "solution"
 end
 
--- example of `benchmark`
-benchmark(solve, 100000)
+helpers.benchmark(solve, 100000)
