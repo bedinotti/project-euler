1d9705e0bafa9bd2da85c9a8c8e0ddf2767a1d04
Chris Downie <cdownie@gmail.com>
2020-01-27 19:38:56 -0700

    [018] Remove failed a* search approach.

--- 018/solution.lua
+++ 018/solution.lua
@@ -2,122 +2,26 @@ local helpers = loadfile("helpers.lua")()
 
 shouldPrintDebug = false
 
--- Define a max heap data structure
--- For now, it's an array with insertion sort. Largest value at the end.
-local Heap = {}
-function Heap:new (o)
-  o = o or {}
-  -- self.utility = self.utility or function (node) return node.value end
-  self.nodes = {}
-  self.__index = self
-  setmetatable(o, self)
-  return o
-end
-
-function Heap:push(node)
-  local value = node.value
-  local depth = node.row
-  local index = 1
-  while index <= #self.nodes and value > self.nodes[index].value do
-    index = index + 1
-  end
-  while index <= #self.nodes and value == self.nodes[index].value and depth > self.nodes[index].row do
-    index = index + 1
-  end
-  table.insert(self.nodes, index, node)
-end
-
-function Heap:pop()
-  if #self.nodes == 0 then return nil end
-  local node = self.nodes[#self.nodes]
-  self.nodes[#self.nodes] = nil
-  return node
-end
-
-function Heap:isEmpty()
-  return #self.nodes == 0
-end
-
 function debug(...)
   if shouldPrintDebug then
     print(...)
   end
 end
 
--- Take the input string and return a tree/table
+-- Take the input string and return a table
 function parseTable(input)
   local rows = {}
   for line in string.gmatch(input, "[%d ]+") do
     local column = {}
     for digits in string.gmatch(line, "%d+") do
       column[#column + 1] = {
-        row = #rows + 1,
-        column = #column + 1,
-        children = {},
         value = tonumber(digits)
       }
     end
     rows[#rows + 1] = column
   end
 
-  for rowIndex=2, #rows do
-    local row = rows[rowIndex]
-    for col=1, #row do
-      local thisNode = rows[rowIndex][col]
-      local previousRowSameColumn = rows[rowIndex-1][col]
-      local previousRowPreviousColumn = rows[rowIndex-1][col-1]
-      if previousRowSameColumn then
-        table.insert(previousRowSameColumn.children, thisNode)
-      end
-
-      if previousRowPreviousColumn then
-        table.insert(previousRowPreviousColumn.children, thisNode)
-      end
-    end
-  end
-
   return rows
-end
-
--- These are methods used for my failed A* search approach.
-function traceAndAccumulate(fromNode)
-  local parent = fromNode["parent"]
-  debug("Tracing ..")
-  local sum = fromNode.value
-  debug(sum)
-  while parent do
-    sum = sum + parent.value
-    debug("+ " .. parent.value, sum)
-    parent = parent["parent"]
-  end
-  return sum
-end
-
-function solveWithAStarSearch(pyramidString)
-  local tree = parseTable(pyramidString)
-  local root = tree[1][1]
-  -- Start at the last row. Add each row above it.
-  local openSet = Heap:new()
-  openSet:push(root)
-
-  while not openSet:isEmpty() do
-    local nextNode = openSet:pop()
-    debug("Popped " .. nextNode.value)
-    if #nextNode.children == 0 then
-      return traceAndAccumulate(nextNode)
-    end
-
-    for childIndex=1, #nextNode.children do
-      local child = nextNode.children[childIndex]
-      if not child.parent then
-        debug("Most expensive parent to " .. child.value .. " is " .. nextNode.value) 
-        child.parent = nextNode
-        openSet:push(child)
-      end
-    end
-  end
-  
-  return 0
 end
 
 function solveWithBottomUpPruning(pyramidString)
@@ -136,7 +40,7 @@ function solveWithBottomUpPruning(pyramidString)
   return rows[1][1].value
 end
 
--- local solve = solveWithAStarSearch
+-- Run the tests, then solve with a benchmark.
 local solve = solveWithBottomUpPruning
 local easyInput = [[3
 7 4
