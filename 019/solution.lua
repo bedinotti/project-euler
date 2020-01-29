05aadf3b10c9b4e43a19720eb6fa1a2b0c3aba4e
Chris Downie <cdownie@gmail.com>
2020-01-28 21:42:27 -0700

    [019] Add lua solution.

--- 019/solution.lua
+++ 019/solution.lua
@@ -1,8 +1,29 @@
 local helpers = loadfile("helpers.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+function solve()
+  local totalDays = 0
+  local firstSundays = 0
+  local daysInAYear = {31, 28, 31, 30, 31, 30, 
+                       31, 31, 30, 31, 30, 31}
+  for year = 1901, 2000 do
+    for month = 1, #daysInAYear do
+      local daysInMonth = daysInAYear[month]
+      if month == 2 then
+        local leapDay = 0
+        if (year % 4 == 0 and year % 100 ~= 0) or (year % 400 == 0) then
+          leapDay = 1
+        end
+        daysInMonth = daysInMonth + leapDay
+      end
+
+      if totalDays % 7 == 0 then
+        firstSundays = firstSundays + 1
+      end
+      totalDays = totalDays + daysInMonth
+    end
+  end
+
+  return firstSundays
 end
 
 helpers.benchmark(solve, 100000)
