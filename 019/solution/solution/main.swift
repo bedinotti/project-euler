350659184813073393d7e9f6666cc2a8e4fe09d8
Chris Downie <cdownie@gmail.com>
2020-01-28 21:12:34 -0700

    [019] Add problem statement and template solutions.

---
+++ 019/solution/solution/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  solution
+//
+//  Created by Chris Downie on 1/19/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+/// Measure how long it takes to execute the `method` closure
+/// - Parameter method: The method to benchmark.
+func benchmark(method: () -> Void) {
+    let start = Date().timeIntervalSinceReferenceDate
+    method()
+    let end = Date().timeIntervalSinceReferenceDate
+    print(String(format: "Solved in %.4fs", end - start))
+}
+
+benchmark {
+    for i in 0..<1_000_000 {
+        print(i)
+    }
+}
