ee7691684bb7807376985b0f2bd264cbe23de03b
Chris Downie <cdownie@gmail.com>
2020-01-29 19:00:41 -0700

    [020] Add instant lua solution. Euler says it's incorrect...

--- 020/solution.lua
+++ 020/solution.lua
@@ -1,8 +1,26 @@
 local helpers = loadfile("helpers.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+function factorial(n, product)
+  if n == 1 then
+    return product
+  end
+  return factorial(n - 1, product * n)
 end
 
-helpers.benchmark(solve, 100000)
+function sumDigitsIn(number)
+  local str = string.format("%.f", number)
+  local sum = 0
+  for i=1, #str do
+    sum = sum + tonumber(string.sub(str, i, i))
+  end
+  return sum
+end
+
+function factorialDigitSum(n)
+  local product = factorial(n, 1.0)
+  return sumDigitsIn(product)
+end
+
+helpers.expect(27, factorialDigitSum, 10)
+
+helpers.benchmark(factorialDigitSum, 100)
