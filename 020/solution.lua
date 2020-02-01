96c82792045881a4678f17b5e55e74b81dd388bc
Chris Downie <cdownie@gmail.com>
2020-01-31 21:23:41 -0700

    [020] Implement BigInt multiply in lua.

--- 020/solution.lua
+++ 020/solution.lua
@@ -29,22 +29,59 @@ function BigInt.add (lhs, rhs)
     local digit = lhs:place(i) + rhs:place(i) + carry
     if digit < 9 then
       carry = 0
-      sum = sum .. string.format("%d", digit)
+      sum = string.format("%d", digit) .. sum
     else
       carry = math.floor(digit / 10)
-      sum = sum .. string.format("%d", digit % 10)
+      sum = string.format("%d", digit % 10) .. sum
     end
   end
   if carry > 0 then
-    sum = sum .. string.format("%d", carry)
+    sum = string.format("%d", carry) .. sum
   end
   return BigInt.new(sum)
 end
 bigint_mt.__add = BigInt.add
 
 function BigInt.multiply (lhs, rhs)
-  -- Another poor implementation
-  return lhs
+  local terms = {}
+  local longerNumber
+  local shorterNumber
+  if #lhs.value > #rhs.value then
+    longerNumber = lhs
+    shorterNumber = rhs
+  else 
+    longerNumber = rhs
+    shorterNumber = lhs
+  end
+
+  for i=1, #shorterNumber.value do
+    local carry = 0
+    local term = ""
+    for j=1, i-1 do term = term .. "0" end
+    for j=1, #longerNumber.value do
+      local digit = longerNumber:place(j) * shorterNumber:place(i) + carry
+
+      if digit < 9 then
+        carry = 0
+        term = string.format("%d", digit) .. term
+      else
+        carry = math.floor(digit / 10)
+        term = string.format("%d", digit % 10) .. term
+      end
+    end
+
+    if carry > 0 then
+      term = string.format("%d", carry) .. term
+    end
+    terms[#terms + 1] = BigInt.new(term)
+  end
+
+  local sum = BigInt.new(0)
+  for i=1, #terms do
+    sum = sum + terms[i]
+  end
+
+  return sum
 end
 bigint_mt.__mul = BigInt.multiply
 
@@ -53,6 +90,7 @@ function BigInt.equal (lhs, rhs)
 end
 bigint_mt.__eq = BigInt.equal
 
+-- Begin the main problem solving, with Big Ints
 function factorial(n, product)
   if n == 1 then
     return product
@@ -89,7 +127,13 @@ helpers.expect(0, function () return BigInt.new(2):place(6) end)
 print("\nAdd tests for BigInt")
 helpers.expect(BigInt.new(2), function () return BigInt.new(1) + BigInt.new(1) end)
 helpers.expect(BigInt.new(11), function () return BigInt.new(5) + BigInt.new(6) end)
+helpers.expect(BigInt.new(2244), function () return BigInt.new(1010) + BigInt.new(1234) end)
 helpers.expect(BigInt.new(4), function () return BigInt.new(2) + BigInt.new(2) end)
+
+print("\nMultiply tests for BigInt")
+helpers.expect(BigInt.new(2), function () return BigInt.new(1) * BigInt.new(2) end)
+helpers.expect(BigInt.new(30), function () return BigInt.new(5) * BigInt.new(6) end)
+helpers.expect(BigInt.new(603153), function () return BigInt.new(753) * BigInt.new(801) end)
 
 -- Tests for the whole solution
 -- helpers.expect(27, factorialDigitSum, 10)
