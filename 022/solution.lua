b98ac148675244107c85619ba6b390ad3cab6325
Chris Downie <cdownie@gmail.com>
2020-02-02 08:35:39 -0700

    [022] Lua: Add file loading method.

--- 022/solution.lua
+++ 022/solution.lua
@@ -1,5 +1,6 @@
 local helpers = loadfile("helpers.lua")()
 
+-- Compute the value of a name at a given position in the list
 function valueOf(name, position)
   position = position or 1
   name = string.upper(name)
@@ -11,6 +12,22 @@ function valueOf(name, position)
   return sum * position
 end
 
+-- Load a list of names given the filename
+function loadList(filename)
+  io.input(filename)
+  local input = io.read("all")
+  local names = {}
+  print(input)
+  for name in string.gmatch(input, "\"%a+\"") do
+    print("Found name:" .. name)
+    names[#names + 1] = name
+  end
+  return names
+end
+
 print("\nTest the name evaluation function")
 helpers.expect(53, valueOf, "COLIN")
 helpers.expect(49714, valueOf, "COLIN", 938)
+
+print("\nTest file loading")
+helpers.expect(7, function(f) return #loadList(f) end, "small_names.txt")
