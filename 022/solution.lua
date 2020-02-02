d9c0ff0ca1e293a4395cd2a0763692302bf617f0
Chris Downie <cdownie@gmail.com>
2020-02-02 08:02:38 -0700

    [022] Lua: Implement name valueOf function. Passes tests.

--- 022/solution.lua
+++ 022/solution.lua
@@ -1,8 +1,16 @@
 local helpers = loadfile("helpers.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+function valueOf(name, position)
+  position = position or 1
+  name = string.upper(name)
+  local capitalAOffset = 64 -- 65 in ascii - 1 to keep it one indexed
+  local sum = 0
+  for i=1, #name do
+    sum = sum + string.byte(name, i) - capitalAOffset
+  end
+  return sum * position
 end
 
-helpers.benchmark(solve, 100000)
+print("\nTest the name evaluation function")
+helpers.expect(53, valueOf, "COLIN")
+helpers.expect(49714, valueOf, "COLIN", 938)
