998f94ef47838b0533c36df1e9629e9a105cbb33
Chris Downie <cdownie@gmail.com>
2020-02-04 20:51:00 -0700

    [023] Remove unused code from lua solution.

--- 023/solution.lua
+++ 023/solution.lua
@@ -38,49 +38,41 @@ function isAbundant(number)
   return result
 end
 
--- return true if a number is the sum of two abundant numbers. 
-function isSumOfTwoAbundantNumbers(number)
-  for i=1, math.floor(number / 2) + 1 do
-    local biggerHalf = number - i
-    if i ~= biggerHalf and isAbundant(i) and isAbundant(biggerHalf) then
-      return true
-    end
-  end
-  return false
-end
-
 -- This is a terrible name, but suits the problem
 function sumOfNotAbundantSumNumbers()
   local abundantSumsAreAllAtOrAbove = 28123
 
+  local abundantList = {}
+  for i=1, abundantSumsAreAllAtOrAbove do
+    if isAbundant(i) then
+      abundantList[#abundantList + 1] = i
+    end
+  end
+  helpers.ping("Abundant list generated")
+
+  local pairAbundantSet = summedPairsIn(abundantList)
+  helpers.ping("Abundant pairwise sums generated")
+
   local sum = 0
   for i=1, abundantSumsAreAllAtOrAbove do
-    if not isSumOfTwoAbundantNumbers(i) then
-      -- print(i, "is not a sum of 2 abundant numbers")
+    if not pairAbundantSet[i] then
       sum = sum + i
-    end
-
-    if i % 4000 == 0 then
-      helpers.ping(i .. " of " .. abundantSumsAreAllAtOrAbove)
     end
   end
   return sum
 end
 
--- Fast pasters
--- Summing all pairs in {a b c d}
---    becomes (a + b) + (a + c) + (a + d) + (b + c) + (b + d) + (c + d)
---    becomes 3a + 3b + 3c + 3d
---    becomes 3 (a + b + c + d)
---    becomes (n-1) * (a + b + ... + n) 
-function sumOfAllPairs(pairList)
-  local sum = 0
-  for i = 1, #pairList do
-    sum = sum + pairList[i]
+function summedPairsIn(pairList)
+  local sums = {}
+  for i=1, #pairList do
+    for j=i, #pairList do
+      sums[pairList[i] + pairList[j]] = true
+    end
   end
-  return sum * (#pairList - 1)
+  return sums
 end
 
+-- Begin tests
 testGroup{
   title = "isAbundant",
   tests = {
@@ -93,24 +85,5 @@ for i=1, 11 do
   helpers.expect(false, isAbundant, i)
 end
 
-print("\nTesting isSumOfTwoAbundantNumbers")
-for i=28124, 28150 do
-  helpers.expect(true, isSumOfTwoAbundantNumbers, i)
-end
-
--- testGroup{
---   title = "sumOfAllPairs",
---   tests = {
---     {12, sumOfAllPairs, {1, 2, 3}}
---   }
--- }
-
--- helpers.expect(false, isAbundant, 28)
--- helpers.expect(true, isAbundant, 12)
-
+--solve the problem
 helpers.benchmark(sumOfNotAbundantSumNumbers)
-
--- wrong answers
--- 4112735
--- 4140858
--- 4179935
