dd6f7bc1b44fa34034637c2561cc9c0edad63033
Chris Downie <cdownie@gmail.com>
2020-02-04 22:05:58 -0700

    [024] Add lua solution. Solves in 23s

--- 024/solution.lua
+++ 024/solution.lua
@@ -1,8 +1,28 @@
 local helpers = loadfile("helpers.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+function orderedPermutationsOf(digitList)
+  if #digitList == 1 then
+    return {tostring(digitList[1])}
+  end
+
+  local result = {}
+  for i=1, #digitList do
+    local digitToPrepend = digitList[i]
+    local copy = {table.unpack(digitList)}
+    table.remove(copy, i)
+    local otherPerms = orderedPermutationsOf(copy)
+
+    for j=1, #otherPerms do
+      result[#result + 1] = digitToPrepend .. otherPerms[j]
+    end
+  end
+
+  return result
 end
 
-helpers.benchmark(solve, 100000)
+function millionthPermutation()
+  local result = orderedPermutationsOf{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+  return result[1000000]
+end
+
+helpers.benchmark(millionthPermutation)
