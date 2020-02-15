0ca1338a8e8cff0f68fcca5256cba8fdb4113e47
Chris Downie <cdownie@gmail.com>
2020-02-15 16:25:32 -0700

    [026] Add swift solution. Solves in 70ms.

--- 026/solution/solution/main.swift
+++ 026/solution/solution/main.swift
@@ -18,6 +18,66 @@ func benchmark<T>(method: () -> T) {
     print(result)
 }
 
+func lengthOfRepeatingDecimal(for denominator: Int) -> Int {
+    let decimal = repeatingDecimal(for: denominator)
+    guard let openParenPosition = decimal.firstIndex(of: "("),
+        let closeParenPosition = decimal.lastIndex(of: ")") else {
+            return 0
+    }
+    return decimal[openParenPosition...closeParenPosition].count - 2
+}
+
+func repeatingDecimal(for denominator: Int) -> String {
+    var carry = ""
+    var carriesSeen = [String:Int]()
+    var decimalPlace: Int?
+    var result = ""
+
+    var index = 0
+    while carriesSeen[carry] == nil {
+        carriesSeen[carry] = index
+        
+        let digit = index == 0 ? 1 : 0
+        if index > 0 && decimalPlace == nil {
+            decimalPlace = index
+        }
+        let numeratorDigit = Int("\(carry)\(digit)")!
+        let dividesEvenly = numeratorDigit / denominator
+        let dividesRemainder = numeratorDigit % denominator
+        
+        carry = "\(dividesRemainder)"
+        if decimalPlace == index {
+            result.append(".\(dividesEvenly)")
+        } else {
+            result.append("\(dividesEvenly)")
+        }
+
+        index += 1
+    }
+    
+    let splitPoint = carriesSeen[carry]! + 1
+    let splitIndex = result.index(result.startIndex, offsetBy: splitPoint)
+    let beforeParen = result[result.startIndex..<splitIndex]
+    let afterParen = result[splitIndex..<result.endIndex]
+    result = "\(beforeParen)(\(afterParen))"
+    result = result.replacingOccurrences(of: "(0)", with: "")
+    return result
+}
+
+assert(lengthOfRepeatingDecimal(for: 2) == 0)
+assert(lengthOfRepeatingDecimal(for: 3) == 1)
+assert(lengthOfRepeatingDecimal(for: 6) == 1)
+assert(lengthOfRepeatingDecimal(for: 7) == 6)
+assert(lengthOfRepeatingDecimal(for: 10) == 0)
+
 benchmark {
-    (0..<1_000_000).reduce(0, +)
+    (2..<1_000)
+        .map { denominator -> (Int, Int) in
+            (denominator, lengthOfRepeatingDecimal(for: denominator))
+        }.reduce((-1, -1)) { (lastTuple, thisTuple) -> (Int, Int) in
+            if lastTuple.1 > thisTuple.1 {
+                return lastTuple
+            }
+            return thisTuple
+        }.0
 }
