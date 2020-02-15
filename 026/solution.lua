e8445b62e293930791e39b8da125ce6fdaecbe13
Chris Downie <cdownie@gmail.com>
2020-02-15 16:29:55 -0700

    [026] Remove unused lua code. Still solves in 118ms.

--- 026/solution.lua
+++ 026/solution.lua
@@ -61,38 +61,6 @@ function BigInt.tostring (bigint)
 end
 lib.bigint_mt.__tostring = BigInt.tostring
 
-function recurringLengthFor(denominator)
-  local digits = ""
-  local repeatCount = 0
-  for powerOfTen = 1, math.huge do
-    local nextDigit = ((10 ^ powerOfTen) / denominator) % 10
-    if nextDigit == 0 then
-      return #(shortestRepeatingString(digits) or "")
-    end
-    digits = digits .. string.format("%.f", nextDigit)
-    repeatCount = #(shortestRepeatingString(digits) or "")
-    if repeatCount > 0 then
-      return repeatCount
-    end
-  end
-  return 0
-end
-
-function shortestRepeatingString(inString)
-  if #inString < 2 then
-    return nil
-  end
-  for startIndex = 1, #inString do
-    local midpoint = math.floor((startIndex + #inString) / 2)
-    local stringToTest = string.sub(inString, startIndex, midpoint)
-    if stringToTest == string.sub(inString, midpoint+1, #inString) then 
-      -- try to find a smaller string
-      return shortestRepeatingString(stringToTest) or stringToTest
-    end
-  end
-  return nil
-end
-
 function longestRecurringDenominator(upTo)
   local longestRecurringLength = 0
   local longestDenominator = 0
@@ -114,36 +82,11 @@ testGroup {
   "BigInt division",
   tests = {
     {BigInt.new(1),            function (d) return BigInt.new(1) / d end, BigInt.new(1)},
-    {BigInt.new("0.5"),            function (d) return BigInt.new(1) / d end, BigInt.new(2)},
+    {BigInt.new("0.5"),        function (d) return BigInt.new(1) / d end, BigInt.new(2)},
     {BigInt.new("0.(3)"),      function (d) return BigInt.new(1) / d end, BigInt.new(3)},
     {BigInt.new("0.25"),       function (d) return BigInt.new(1) / d end, BigInt.new(4)},
     {BigInt.new("0.1(6)"),     function (d) return BigInt.new(1) / d end, BigInt.new(6)},
     {BigInt.new("0.(142857)"), function (d) return BigInt.new(1) / d end, BigInt.new(7)}
-  }
-}
-
-testGroup {
-  "shortestRepeatingString",
-  skip = true,
-  tests = {
-    {nil, shortestRepeatingString, "5"},
-    {"5", shortestRepeatingString, "55"},
-    {"5", shortestRepeatingString, "555555"},
-    {"3", shortestRepeatingString, "163333"},
-    {"142857", shortestRepeatingString, "142857142857"},
-    {"142857", shortestRepeatingString, "142857142857142857"},
-  }
-}
-
-testGroup {
-  "recurringLengthFor",
-  skip = true,
-  tests = {
-    {0, recurringLengthFor, 2},
-    {1, recurringLengthFor, 3},
-    {1, recurringLengthFor, 6},
-    {6, recurringLengthFor, 7},
-    {0, recurringLengthFor, 10}
   }
 }
 
