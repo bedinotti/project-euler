3098191b9c6d8bb799595a0e955d3c1d483a04c7
Chris Downie <cdownie@gmail.com>
2020-02-16 16:10:27 -0700

    [028] Solves in Lua in < 1ms.

--- 028/solution.lua
+++ 028/solution.lua
@@ -2,8 +2,35 @@ local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
 -- Example solution function
-function solve(arg1)
-  return "solution"
+function diagonalSum(sideLength)
+  local lastSeenCell = 0
+  local sum = 0
+
+  for side = 1, sideLength, 2 do
+    if side == 1 then
+      -- This is special, because all four corners are the same number, counted only once.
+      lastSeenCell = 1
+      sum = sum + lastSeenCell
+    else
+      local bottomRight = side + lastSeenCell - 1
+      local bottomLeft = bottomRight + side - 1
+      local topLeft = bottomLeft + side - 1
+      local topRight = topLeft + side - 1
+      lastSeenCell = topRight
+      sum = sum + bottomRight + bottomLeft + topLeft + topRight
+    end
+  end
+
+  return sum
 end
 
-helpers.benchmark(solve, 100000)
+helpers.testGroup {
+  "diagonalSum",
+  tests = {
+    {1, diagonalSum, 1},
+    {25, diagonalSum, 3},
+    {101, diagonalSum, 5}
+  }
+}
+
+helpers.benchmark(diagonalSum, 1001)
