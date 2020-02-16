f804fb6bb4ff8d7884a8b96125b9ac9b38bdb9ff
Chris Downie <cdownie@gmail.com>
2020-02-16 08:16:10 -0700

    [027] Implement Swift solution, same wrong answer in 3.2s

--- 027/solution/solution/main.swift
+++ 027/solution/solution/main.swift
@@ -18,6 +18,66 @@ func benchmark<T>(method: () -> T) {
     print(result)
 }
 
-benchmark {
-    (0..<1_000_000).reduce(0, +)
+func isPrime(_ n: Int) -> Bool {
+    if n == 1 {
+        return false
+    }
+    if n == 2 {
+        return true
+    }
+    for i in 2..<max(n / 2 + 1, 2)  {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func formulaFor(a: Int, b: Int) -> (Int) -> Int {
+    { n in
+        (n * n) + (a * n) + b
+    }
+}
+
+func primeCount(a: Int, b: Int) -> Int {
+    let quadatricFormula = formulaFor(a: a, b: b)
+    
+    let firstNonPrimeN = (0...).first { n -> Bool in
+         !isPrime(quadatricFormula(n))
+    }
+    return (firstNonPrimeN ?? 0)
+}
+
+assert(primeCount(a: 1, b: 41) == 40)
+assert(primeCount(a: -79, b: 1601) == 80)
+
+func coefficientsWithMostConsecutivePrimes(aMaxExclusive: Int, bMaxInclusive: Int) -> (Int, Int) {
+    var pairs = [(Int, Int)]()
+    
+    let aBound = aMaxExclusive > 0 ? aMaxExclusive - 1 : 0
+    let aLowerBound = -aBound
+    let bLowerBound = -bMaxInclusive
+    
+    (aLowerBound...aBound).forEach { aValue in
+        (bLowerBound...bMaxInclusive).forEach { bValue in
+            pairs.append((aValue, bValue))
+        }
+    }
+    
+    var bestPair: (Int, Int)?
+    var primeCountForBestPair = 0
+    pairs.forEach { pair in
+        let count = primeCount(a: pair.0, b: pair.1)
+        if count > primeCountForBestPair {
+            bestPair = pair
+            primeCountForBestPair = count
+        }
+    }
+    
+    return bestPair!
+}
+
+benchmark { () -> Int in
+    let coefficientPair = coefficientsWithMostConsecutivePrimes(aMaxExclusive: 1000, bMaxInclusive: 1000)
+    return coefficientPair.0 * coefficientPair.1
 }
