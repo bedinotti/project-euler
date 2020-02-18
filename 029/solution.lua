af76c2fb160ed94dec11d618c6f1b2f69ad75c02
Chris Downie <cdownie@gmail.com>
2020-02-17 18:33:13 -0700

    [029] Add found failing tests of 93 ^ 99 and 93 ^ 100

--- 029/solution.lua
+++ 029/solution.lua
@@ -10,6 +10,10 @@ function BigInt.pow (num, exp)
   for i=1, tonumber(exp.value) do
     product = product * num
   end
+  -- if #product.value > 200 then
+  --   print(string.format("%s ^ %s thinks its %s", num.value, exp.value, product.value))
+  -- end
+
   return product
 end
 lib.bigint_mt.__pow = BigInt.pow
@@ -20,7 +24,25 @@ helpers.testGroup {
     {BigInt.new(1),    function(a, b) return BigInt.new(a) ^ BigInt.new(b) end, 3, 0},
     {BigInt.new(3),    function(a, b) return BigInt.new(a) ^ BigInt.new(b) end, 3, 1},
     {BigInt.new(3125), function(a, b) return BigInt.new(a) ^ BigInt.new(b) end, 5, 5},
-    {BigInt.new(64),   function(a, b) return BigInt.new(a) ^ BigInt.new(b) end, 4, 3}
+    {BigInt.new(64),   function(a, b) return BigInt.new(a) ^ BigInt.new(b) end, 4, 3},
+    {
+      BigInt.new("758249105831845933594546715344288617476769120250268508890936189254587920359456781681256381635348220868544500971808346750820604361621064506588359942316662865507114612000524007989494784832520237957"),
+      function(a, b) return BigInt.new(a) ^ BigInt.new(b) end,
+      93,
+      99
+    },
+    {
+      BigInt.new("70517166842361671824292844527018841425339528183274971326857065600676676593429480696356843492087384540774638590378176247826316205630758999112717474635449646492161658916048732743023014989424382130001"),
+      function(a, b) return BigInt.new(a) ^ BigInt.new(b) end,
+      93,
+      100
+    },
+    {
+      BigInt.new("100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
+      function(a, b) return BigInt.new(a) ^ BigInt.new(b) end,
+      100,
+      100
+    }
   }
 }
 
@@ -37,11 +59,21 @@ function distinctPowers(maxA, maxB)
     end
   end
 
-  local count = 0
+  local keys = {}
   for k, _ in pairs(seenValues) do
-    count = count + 1
+    keys[#keys + 1] = k
   end
-  return count
+  table.sort(keys, function (a, b)
+    if #a < #b then
+      return true
+    elseif #b < #a then
+      return false
+    else
+      return a < b
+    end
+  end)
+  -- print(table.unpack(keys))
+  return #keys
 end
 
 helpers.testGroup {
@@ -51,4 +83,4 @@ helpers.testGroup {
   }
 }
 
-helpers.benchmark(distinctPowers, 100, 100)
+-- helpers.benchmark(distinctPowers, 100, 100)
