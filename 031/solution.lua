c5225f26b1d84fd91d5ab3ae1aaa64d53cb621fd
Chris Downie <cdownie@gmail.com>
2020-03-21 17:24:36 -0600

    [031] Use back-to-front iteration in Lua. Solves in 38ms

--- 031/solution.lua
+++ 031/solution.lua
@@ -3,26 +3,22 @@ local lib = loadfile("library.lua")()
 
 -- Example solution function
 function permuatationCount(values, targetValue)
-  -- print("permCount, values:" .. table.concat(values, ",") .. " tv:" .. targetValue)
   if targetValue == 0 and #values == 0 then
-    -- this base case is odd. Not sure about it. Leaving it for now.
-    -- print("returning 1")
     return 1
   elseif targetValue < 0 then
-    -- print("returning 0")
     return 0
   end
 
   local count = 0
 
-  local remainingValues = table.pack(table.unpack(values))
-  if #remainingValues > 0 then
-    local value = remainingValues[1]
-    table.remove(remainingValues, 1)
+  if #values > 0 then
+    local remainingValues = table.pack(table.unpack(values))
+    -- Use the last value to reduce index updates
+    local value = remainingValues[#remainingValues]
+    table.remove(remainingValues, #remainingValues)
 
     local valueMultiple = 0
     while valueMultiple * value <= targetValue do
-      -- print("trying " .. valueMultiple .. " * " .. value)
       count = count + permuatationCount(remainingValues, targetValue - valueMultiple * value)
       valueMultiple = valueMultiple + 1
     end
