40f0610e91fc29433569cde245e5b90b996688d7
Chris Downie <cdownie@gmail.com>
2020-03-21 17:20:53 -0600

    [031] Solves it in Lua. runs in 8.7s

--- 031/solution.lua
+++ 031/solution.lua
@@ -3,27 +3,29 @@ local lib = loadfile("library.lua")()
 
 -- Example solution function
 function permuatationCount(values, targetValue)
-  -- print("permCount called, tv: " .. targetValue)
-  if targetValue == 0 then
+  -- print("permCount, values:" .. table.concat(values, ",") .. " tv:" .. targetValue)
+  if targetValue == 0 and #values == 0 then
     -- this base case is odd. Not sure about it. Leaving it for now.
+    -- print("returning 1")
     return 1
   elseif targetValue < 0 then
+    -- print("returning 0")
     return 0
   end
 
   local count = 0
 
-  for i=1, #values do
-    count = count + permuatationCount(values, targetValue - values[i])
+  local remainingValues = table.pack(table.unpack(values))
+  if #remainingValues > 0 then
+    local value = remainingValues[1]
+    table.remove(remainingValues, 1)
 
-    -- local value = values[i]
-    -- print("Examining " .. value)
-
-    -- local remainingTarget = targetValue
-    -- while remainingTarget >= value do
-    --   remainingTarget = remainingTarget - value
-    --   count = count + permuatationCount(values, remainingTarget)
-    -- end
+    local valueMultiple = 0
+    while valueMultiple * value <= targetValue do
+      -- print("trying " .. valueMultiple .. " * " .. value)
+      count = count + permuatationCount(remainingValues, targetValue - valueMultiple * value)
+      valueMultiple = valueMultiple + 1
+    end
   end
   
   return count
@@ -38,4 +40,4 @@ helpers.testGroup {
   }
 }
 
--- helpers.benchmark(permuatationCount, {1, 2, 5, 10, 20, 50, 100, 200})
+helpers.benchmark(permuatationCount, {1, 2, 5, 10, 20, 50, 100, 200}, 200)
