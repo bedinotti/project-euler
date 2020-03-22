341df51ee5cd55dddb8ccc1a1c3fbd7e5d272d07
Chris Downie <cdownie@gmail.com>
2020-03-22 17:41:33 -0600

    [034] In Lua, write factorial method with tests.

--- 034/solution.lua
+++ 034/solution.lua
@@ -1,9 +1,38 @@
 local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+-- Factorial implementation with tail recursion
+function factorial(n, product)
+  product = product or 1
+  if n < 1 then
+    return 1
+  elseif n == 1 then
+    return product
+  end
+
+  return factorial(n - 1, product * n)
 end
 
-helpers.benchmark(solve, 100000)
+helpers.testGroup {
+  "factorial",
+  tests = {
+    {1, factorial, 1},
+    {24, factorial, 4},
+    {120, factorial, 5},
+  }
+}
+
+function isCurious(number)
+  return false
+end
+
+helpers.testGroup {
+  "isCurious",
+  tests = {
+    {true, isCurious, 145},
+    {false, isCurious, 1},
+    {false, isCurious, 2}
+  }
+}
+
+-- helpers.benchmark(solve, 100000)
