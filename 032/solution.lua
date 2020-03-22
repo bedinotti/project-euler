0fcc23717067ffa5f4dba599a5c8a7c67c00303e
Chris Downie <cdownie@gmail.com>
2020-03-21 20:43:52 -0600

    [032] Add test for pandigital product, with tests in lua.

--- 032/solution.lua
+++ 032/solution.lua
@@ -1,9 +1,43 @@
 local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
--- Example solution function
-function solve(arg1)
-  return "solution"
+function isPandigitalProduct(x, y)
+  local product = x * y
+  local allDigits = string.format("%d%d%d", x, y, product)
+
+  local digitTable = {}
+  for i=1, #allDigits do
+    digitTable[i] = string.sub(allDigits, i, i)
+  end
+  table.sort(digitTable)
+
+  local orderedString = table.concat(digitTable)
+  return orderedString == "123456789"
 end
 
-helpers.benchmark(solve, 100000)
+helpers.testGroup {
+  "isPandigitalProduct",
+  tests = {
+    {true, isPandigitalProduct, 39, 186},
+    -- same product, but different digits
+    {false, isPandigitalProduct, 78, 93},
+  }
+}
+
+
+-- Example solution function
+function sumAllPandigitalProducts()
+  local availableDigits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+  -- choose X, choose Y
+
+  -- multiply, test result
+
+  -- add product to a table as a key
+
+  -- count keys
+
+  return 0
+end
+
+helpers.benchmark(sumAllPandigitalProducts)
