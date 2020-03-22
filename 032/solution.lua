477a6e9bcb5fad12935eb2b97339d6a6aa8b83e5
Chris Downie <cdownie@gmail.com>
2020-03-21 21:15:57 -0600

    [032] Refactor multiplicands problem into coroutine in lua.

--- 032/solution.lua
+++ 032/solution.lua
@@ -24,20 +24,39 @@ helpers.testGroup {
   }
 }
 
+function multiplicands()
+  local guessedMax = 1000
+  return coroutine.wrap(function () 
+    for x=1, guessedMax do
+      for y=1, guessedMax do
+        coroutine.yield(x, y)
+      end
+    end
+  end)
+end
 
 -- Example solution function
 function sumAllPandigitalProducts()
   local availableDigits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+  local guessedMax = 987654321
 
+  local products = {}
   -- choose X, choose Y
-
-  -- multiply, test result
-
-  -- add product to a table as a key
+  -- this is dumb and slow. But also boring. And easy to write
+  for x, y in multiplicands() do
+    if isPandigitalProduct(x, y) then
+      print(x .. "*" .. y .. " is pandigital!")
+      products[x * y] = true
+    end
+  end
 
   -- count keys
+  local sum = 0
+  for key, _ in pairs(products) do
+    sum = sum + key
+  end
 
-  return 0
+  return sum
 end
 
 helpers.benchmark(sumAllPandigitalProducts)
