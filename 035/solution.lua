ef917d554b8b43d4884715ff09ce6682ea39d9d4
Chris Downie <cdownie@gmail.com>
2020-03-28 16:50:18 -0600

    [035] Write slow prime test in lua.

--- 035/solution.lua
+++ 035/solution.lua
@@ -1,6 +1,26 @@
 local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
+-- Slow prime test
+function isPrime(n)
+  if n <= 1 then return false end
+  for i=2, n-1 do
+    if n % i == 0 then 
+      return false
+    end
+  end
+  return true
+end
+
+helpers.testGroup {
+  "isPrime",
+  tests = {
+    {true, isPrime, 2},
+    {true, isPrime, 97},
+    {false, isPrime, 91}
+  }
+}
+
 -- Example solution function
 function solve(arg1)
   return "solution"
