59020f37410e66db0d18f658f0df8a5941eccd33
Chris Downie <cdownie@gmail.com>
2020-03-28 17:35:59 -0600

    [035] Use a prime sieve to solve Lua in 0.37s

--- 035/solution.lua
+++ 035/solution.lua
@@ -1,14 +1,44 @@
 local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
--- Slow prime test
+is_prime = {}
+
+-- Recycling primesLessThan from problem 12
+function primesLessThan(upperBound)
+  return coroutine.wrap(function ()
+    local allNumbers = {}
+    for i = 1, upperBound - 1 do
+      allNumbers[#allNumbers + 1] = i
+    end
+    local maxIndex = #allNumbers
+
+    local index = 2
+    repeat
+      local nextPrime = allNumbers[index]
+      coroutine.yield(nextPrime)
+
+      for multiples = 2 * nextPrime, maxIndex, nextPrime do
+        allNumbers[multiples] = nil
+      end
+
+      repeat
+        index = index + 1
+      until allNumbers[index] ~= nil or index > maxIndex
+    until index > maxIndex
+  end)
+end
+
+-- Prime test by trial division, memoized
 function isPrime(n)
+  if is_prime[n] then return is_prime[n] end
   if n <= 1 then return false end
   for i=2, n-1 do
-    if n % i == 0 then 
+    if n % i == 0 then
+      is_prime[n] = false
       return false
     end
   end
+  is_prime[n] = true
   return true
 end
 
@@ -49,17 +79,43 @@ helpers.testGroup{
 -- Example solution function
 function circularPrimesBelow(maxN)
   local count = 0
-  for i=2, maxN - 1 do
-    if isCircularPrime(i) then
-      count = count + 1
-    end
+
+  local allPrimes = {}
+  local primeHash = {}
+  local circularPrimes = {}
+
+  -- Populate our list of all primes, and an O(n) hash for prime testing.
+  for i in primesLessThan(maxN) do
+    allPrimes[#allPrimes + 1] = i
+    primeHash[i] = true
   end
-  return count
+
+  for i=1, #allPrimes do
+    local prime = allPrimes[i]
+    -- print("Testing if " .. prime .. " is a circular prime")
+    local nString = tostring(prime)
+    for remainingRotations = #nString - 1, 1, -1 do
+      nString = string.sub(nString, #nString, #nString) .. string.sub(nString, 1, #nString - 1, #nString - 1)
+      if not primeHash[tonumber(nString)] then
+        -- print(nString .. " is not prime")
+        goto nextPrime
+      end
+    end
+
+    -- print("found: " .. prime)
+    circularPrimes[#circularPrimes + 1] = prime
+
+    ::nextPrime::
+    -- print("trying next prime..")
+  end
+
+  return #circularPrimes
 end
 
 helpers.testGroup{
   "circularPrimesBelow",
   tests = {
+    {4, circularPrimesBelow, 10},
     {13, circularPrimesBelow, 100}
   }
 }
