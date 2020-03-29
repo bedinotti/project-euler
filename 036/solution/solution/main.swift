2367aeeb703c1e66bd4e4b9237433b43f0642f73
Chris Downie <cdownie@gmail.com>
2020-03-28 19:44:06 -0600

    [036] Add Swift solution, solves in 0.96s

--- 036/solution/solution/main.swift
+++ 036/solution/solution/main.swift
@@ -18,6 +18,50 @@ func benchmark<T>(method: () -> T) {
     print(result)
 }
 
+func isPalindrome(_ string: String) -> Bool {
+    string == String(string.reversed())
+}
+
+let binaryHash: [Character: String] = [
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    "A": "1010",
+    "B": "1011",
+    "C": "1100",
+    "D": "1101",
+    "E": "1110",
+    "F": "1111",
+]
+
+func binaryString(from number: Int) -> String {
+    let str = String(format: "%X", number).map { binaryHash[$0]! }.reduce("", +)
+    if let firstOne = str.firstIndex(of: "1") {
+        return String(str[firstOne...])
+    }
+    return str
+}
+
+assert(binaryString(from: 10) == "1010")
+assert(binaryString(from: 17) == "10001")
+
+func isDoublePalindrome(number: Int) -> Bool {
+    isPalindrome(String(number)) && isPalindrome(binaryString(from: number))
+}
+
+func sumDoublePalindromesLessThan(_ maxBounds: Int) -> Int {
+    (1..<maxBounds)
+        .filter(isDoublePalindrome(number:))
+        .reduce(0, +)
+}
+
 benchmark {
-    (0..<1_000_000).reduce(0, +)
+    sumDoublePalindromesLessThan(1_000_000)
 }
