d0a621b85a3e6d534e9b2d3ecd4823a3760f3e5e
Chris Downie <cdownie@gmail.com>
2020-03-29 19:23:32 -0600

    [037] Add Swift solution. Runs in 1.1s.

--- 035/solution/solution/main.swift
+++ 035/solution/solution/main.swift
@@ -17,43 +17,7 @@ func benchmark<T>(method: () -> T) {
     print(String(format: "Solved in %.4fs", end - start))
     print(result)
 }
-
-// Recylcing the prime sieve from problem 10
-struct Primes: Sequence, IteratorProtocol {
-    private let maxBounds: Int
-    private var allNumbers: [Int: Bool]
-    private var index: Int
-    
-    init(maxBounds: Int) {
-        self.maxBounds = maxBounds
-        var numbers = [Int: Bool]()
-        (2..<maxBounds).forEach { number in
-            numbers[number] = true
-        }
-        allNumbers = numbers
-        index = 2
-    }
-    
-    mutating func next() -> Int? {
-        guard let isIndexPrime = allNumbers[index], isIndexPrime else {
-            return nil
-        }
-        let nextPrime = index
-        
-        for multiple in stride(from: nextPrime * 2, to: maxBounds, by: nextPrime) {
-            allNumbers[multiple] = false
-        }
-        
-        repeat {
-            index = index + 1
-//            if index % 10_000 == 0 {
-//                print("Comparing index \(index)")
-//            }
-        } while allNumbers[index] == false
-        
-        return nextPrime
-    }
-}
+x
 
 func circularPrimeCount(upperBound: Int) -> Int {
     let primes = Set<Int>(Primes(maxBounds: upperBound))
--- 037/solution/solution/main.swift
+++ 037/solution/solution/main.swift
@@ -18,6 +18,95 @@ func benchmark<T>(method: () -> T) {
     print(result)
 }
 
+
+// Recylcing the prime sieve from problem 35
+struct Primes: Sequence, IteratorProtocol {
+    private let maxBounds: Int
+    private var allNumbers: [Int: Bool]
+    private var index: Int
+    private var seenPrimes = Set<Int>()
+    
+    init(maxBounds: Int) {
+        self.maxBounds = maxBounds
+        var numbers = [Int: Bool]()
+        (2..<maxBounds).forEach { number in
+            numbers[number] = true
+        }
+        allNumbers = numbers
+        index = 2
+    }
+    
+    mutating func next() -> Int? {
+        guard let isIndexPrime = allNumbers[index], isIndexPrime else {
+            return nil
+        }
+        let nextPrime = index
+        
+        for multiple in stride(from: nextPrime * 2, to: maxBounds, by: nextPrime) {
+            allNumbers[multiple] = false
+        }
+        
+        repeat {
+            index = index + 1
+        } while allNumbers[index] == false
+        
+        seenPrimes.insert(nextPrime)
+        return nextPrime
+    }
+    
+    func isPrime(_ n: Int) -> Bool {
+        seenPrimes.contains(n)
+    }
+}
+
+func isBidirectionalTruncatedPrime(_ n: Int, primes: Primes) -> Bool {
+    guard n > 9 else {
+        return false
+    }
+    
+    // truncate right
+    var str = String(n)
+    while !str.isEmpty {
+        if !primes.isPrime(Int(str)!) {
+            return false
+        }
+        str = String(str.dropLast())
+    }
+    
+    // truncate left
+    str = String(n)
+    while !str.isEmpty {
+        if !primes.isPrime(Int(str)!) {
+            return false
+        }
+        str = String(str.dropFirst())
+    }
+    return true
+}
+
+var testPrimes = Primes(maxBounds: 4000)
+while testPrimes.next() != nil {}
+assert(isBidirectionalTruncatedPrime(3797, primes: testPrimes))
+assert(!isBidirectionalTruncatedPrime(19, primes: testPrimes))
+assert(!isBidirectionalTruncatedPrime(3, primes: testPrimes))
+
+func sumElevenBidirectionalTruncatedPrimes(maxGuess: Int = 1_000_000) -> Int {
+    var sequence = Primes(maxBounds: maxGuess)
+    while sequence.next() != nil {}
+    var primes = [Int]()
+    for prime in Primes(maxBounds: maxGuess) {
+        if isBidirectionalTruncatedPrime(prime, primes: sequence) {
+            primes.append(prime)
+            
+            if primes.count == 11 {
+                return primes.reduce(0, +)
+            }
+        }
+    }
+    print("failed", primes)
+    return -1
+}
+
 benchmark {
-    (0..<1_000_000).reduce(0, +)
+    sumElevenBidirectionalTruncatedPrimes()
 }
