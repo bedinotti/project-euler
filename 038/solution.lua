6876892d962e857138fff999e196e6d4fb66f1dc
Chris Downie <cdownie@gmail.com>
2020-04-13 18:48:28 -0600

    [038] Add lua solution, solves in 1.5s.

--- 038/solution.lua
+++ 038/solution.lua
@@ -1,20 +1,24 @@
 local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
--- Example solution function
-function isPandigitalMultiple(number, n)
+function pandigitalProduct(number, n) 
   local products = {}
   for mult=1, n do
     products[#products + 1] = string.format("%d", mult * number)
   end
-  local str = table.concat(products)
+  return table.concat(products)
+end
+
+-- Example solution function
+function isPandigitalMultiple(number, n)
+  local str = pandigitalProduct(number, n)
   local digits = {}
   for i=1, #str do
     digits[#digits + 1] = string.sub(str, i, i)
   end
   table.sort(digits)
 
-  return table.concat(digits) == "123456789"
+  return table.concat(digits) == "123456789", str
 end
 
 helpers.testGroup{
@@ -26,4 +30,21 @@ helpers.testGroup{
   }
 }
 
--- helpers.benchmark(solve, 100000)
+function solve()
+  local largestFound = 0
+
+  local biggestNumberGuess = 10000
+  for i=1, biggestNumberGuess do
+    local biggestNGuess = 10
+    for j=2, biggestNGuess do
+      local success, product = isPandigitalMultiple(i, j)
+      if success then
+        largestFound = math.max(largestFound, tonumber(product))
+      end
+    end
+  end
+
+  return largestFound
+end
+
+helpers.benchmark(solve)
