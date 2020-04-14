938f5b942a1c17afed8de48bd8cfa8ca0b684d17
Chris Downie <cdownie@gmail.com>
2020-04-13 18:13:29 -0600

    [038] Create isPandigitalMultiple test in lua.

--- 038/solution.lua
+++ 038/solution.lua
@@ -2,8 +2,28 @@ local helpers = loadfile("helpers.lua")()
 local lib = loadfile("library.lua")()
 
 -- Example solution function
-function solve(arg1)
-  return "solution"
+function isPandigitalMultiple(number, n)
+  local products = {}
+  for mult=1, n do
+    products[#products + 1] = string.format("%d", mult * number)
+  end
+  local str = table.concat(products)
+  local digits = {}
+  for i=1, #str do
+    digits[#digits + 1] = string.sub(str, i, i)
+  end
+  table.sort(digits)
+
+  return table.concat(digits) == "123456789"
 end
 
-helpers.benchmark(solve, 100000)
+helpers.testGroup{
+  "isPandigitalMultiple",
+  tests = {
+    {true, isPandigitalMultiple, 192, 3},
+    {true, isPandigitalMultiple, 9, 5},
+    {false, isPandigitalMultiple, 3, 3}
+  }
+}
+
+-- helpers.benchmark(solve, 100000)
